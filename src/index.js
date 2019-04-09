855c7699b7abb854027a1ce2c835630ab0ce502a
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2019-04-09 22:54:38 +0200

    Remove unused imports

--- src/index.js
+++ src/index.js
@@ -5,35 +5,16 @@ import App from './App'
 import store from './store'
 import router from './router'
 
-import {
-  Vuetify,
-  VApp,
-  VDivider,
-  VBtn,
-  VGrid,
-  VIcon
-} from 'vuetify'
-
 import 'vuetify/src/stylus/app.styl'
-// import 'vuetify/src/stylus/main.styl'
 import 'vuetify/src/stylus/components/_input-groups.styl'
 
 Vue.config.productionTip = false
-Vue.use(Vuetify, {
-  components: {
-    VApp,
-    VDivider,
-    VBtn,
-    VGrid,
-    VIcon
-  }
-})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   store,
   router,
-  template: '<App/>',
-  components: { App }
+  components: { App },
+  template: '<App/>'
 })
